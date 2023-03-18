f5682a9a189b787ef3809ace0e85229c52d83879
MichelP <michelpr1224@gmail.com>
2023-03-18 16:20:51 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 1489;
+		int b = 829;
+		int c = 828;
+		int d = 1488;
+		System.out.println("Hello World!" + (a*b));*/
+	
+	}
+
+}
